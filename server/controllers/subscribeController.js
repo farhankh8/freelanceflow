c7db91f9d13cda3a3fdb9dd1fa2e8976cb3f7509
farhankh8 <mohammadfarhankh.8@gmail.com>
2026-04-25 18:30:59 +0530

    fix: Remove hardcoded Razorpay fallback keys

--- server/controllers/subscribeController.js
+++ server/controllers/subscribeController.js
@@ -5,8 +5,8 @@ const User = require('../models/User');
 const asyncHandler = require('../middleware/asyncHandler');
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID || 'rzp_test_SfH61mklxoBJWx',
-  key_secret: process.env.RAZORPAY_KEY_SECRET || '8UBO39DQlrn23glGR7cuqlV8'
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET
 });
 
 const PLANS = {
